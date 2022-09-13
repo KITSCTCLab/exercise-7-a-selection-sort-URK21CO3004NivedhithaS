a4c7586d57181f64969943fb1af7c80b2ad47770
URK21CO3004NivedhithaS <110015365+URK21CO3004NivedhithaS@users.noreply.github.com>
2022-09-13 12:19:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,14 @@
-from typing import List
+rom typing import List
 
 def selectionSort(array, size) -> List[int]:
   # Write your code here
+  for i in range(size):
+    min = i
+    for j in range(i + 1, size):
+      if array[min] > array[j]:
+        min = j;
+    array[i], array[min] = array[min], array[i]
+  return array
 
 # Do not change the following code
 input_data = input()
